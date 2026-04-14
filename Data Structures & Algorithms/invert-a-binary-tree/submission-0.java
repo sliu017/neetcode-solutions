b4eb97ed5a836b904f78a80156af3548882c5d3a
Sebastian Liu <132174791+sliu017@users.noreply.github.com>
2026-04-14 17:05:27 -0500

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null)){
+            return root;
+        } else {
+            TreeNode tempLeft = invertTree(root.left);
+            TreeNode tempRight = invertTree(root.right);
+            root.left = root.right;
+            root.right = tempLeft;
+        }
+        return root;
+    }
+}

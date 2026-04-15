372f35852eaecfa9e0e479911b05bdbc3583b2ce
Sebastian Liu <132174791+sliu017@users.noreply.github.com>
2026-04-15 12:42:47 -0500

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+    public int dfs(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        if(l == -1 || r == -1 || Math.abs(l-r) > 1){
+            return -1;
+        }
+        return 1 + Math.max(l,r);
+    }
+}

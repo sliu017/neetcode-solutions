e1e7997e05a99f0a2110aa1e37515368e96c5df7
Sebastian Liu <132174791+sliu017@users.noreply.github.com>
2026-07-01 15:24:18 -0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int x = 0;
+        for(int i = 0; i < nums.length; i++){
+            x ^= nums[i];
+        }
+        for(int i = 0; i <= nums.length; i++){
+            x ^= i;
+        }
+        return x;
+    }
+}
